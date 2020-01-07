f01e70035484bf921b7bba2bdf550d2e71c08f1c
Oliver <oliver37215@hotmail.com>
2020-01-06 17:25:04 -0800

    pre deployment

--- toDoList/settings.py
+++ toDoList/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'pxxum2q=c&bo4)!g6r-*p0sns-x$3679l5@po^t%33bw4c&%!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
